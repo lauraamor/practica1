7f49c6e2d5e858b52bc7f64818e68bafa6ced345
Laura Amor Alonso <lauraamor2001@hotmail.com>
2022-10-17 11:20:54 +0200

    scriptfinal

--- scripts.js
+++ scripts.js
@@ -1,1 +1,15 @@
-
+function grande_breda() {
+    open("/img/breda.jpg");
+}
+function grande_venus() {
+    open("/img/venus.jpg");
+}
+function grande_meninas() {
+    open("/img/meninas.jpg");
+}
+function grande_vulcano() {
+    open("/img/vulcano.jpg");
+}
+function grande_inocenci0(){
+    open("/img/inocencio.jpg");
+}
